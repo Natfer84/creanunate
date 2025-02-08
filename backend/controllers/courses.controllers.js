5670858967f9713a3ef52c886577a3abdb394289
Natfer84 <natita2804@hotmail.com>
2025-02-08 12:46:45 +0100

    me he quedado en getOne

--- backend/controllers/courses.controllers.js
+++ backend/controllers/courses.controllers.js
@@ -16,6 +16,34 @@ export default {
             await mongo.closeClient()
         }
     },
+    getOne: async (req, res) => {
+
+        try {
+            //Conformamos los datos de la solicitud
+            const values = ['id', req.body.id];
+            console.log(values);
+            const course = await genericMongoCrud.getOne(process.env.COLL_COURSES);
+            console.log(course);
+
+            if(course.length === 0){
+                res.status(400).json({message: 'No existe un curso con la ID introducida'})
+            } else {
+                const customResponse = {
+                    code: course[0].code,
+                    name: course[0].name,
+                    start_date: course[0].start_date,
+                    end_date: course[0].end_date,
+                    cover: course[0].cover
+                }
+    
+                res.status(200).json(customResponse);
+            }
+
+        } catch (e) {
+            res.status(400).json({ mesagge: 'Error inesperado al obtener los datos del curso', error: e })
+        }
+    },
+
     nextCourses: async (req, res) => {
         try {
             const result = await genericMongoCrud.getAll(process.env.COLL_NEXCOURSES)
