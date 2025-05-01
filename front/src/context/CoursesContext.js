d3650fea24dd2a04dadffd8d2478c7884dc3b2a0
Natfer84 <natita2804@hotmail.com>
2025-05-01 19:23:32 +0200

    cambiado await fetch http://URI_MONGOLOCAL/creanunate/courses/all-courses

--- front/src/context/CoursesContext.js
+++ front/src/context/CoursesContext.js
@@ -49,7 +49,7 @@ export const CoursesProvider = ({ children }) => {
     const fetchCourses = async () => {
       try {
         const response = await fetch(
-          "http://REACT_APP_API_URL/creanunate/courses/all-courses" // "http://localhost:3001/creanunate/courses/all-courses"
+          "http://URI_MONGOLOCAL/creanunate/courses/all-courses" // "http://localhost:3001/creanunate/courses/all-courses"
         );
         if (!response.ok) {
           throw new Error(
