419e939a5e37e63f2526377773d7437971f00583
Natfer84 <natita2804@hotmail.com>
2025-05-02 10:25:27 +0200

    realizado en el backend en app.js la conexion para que muestre el front desde railway

--- backend/App.js
+++ backend/App.js
@@ -1,16 +1,29 @@
-import express,{ json, urlencoded } from 'express'
-import cors from 'cors'
-import { router } from './routes/routes.js'
+import express, { json, urlencoded } from "express";
+import cors from "cors";
+import { router } from "./routes/routes.js";
+import path from "path"; // para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
+import { fileURLToPath } from "url"; // para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
 
+const app = express();
 
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cors());
 
-const app = express()
+// Aquí configuro para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
 
-app.use(json())
-app.use(urlencoded({ extended: false }))
-app.use(cors())
+// Necesario para usar __dirname con ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(router)
+// servir el frontend con build
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+//
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
+app.use(router);
 
 export default app;
 
