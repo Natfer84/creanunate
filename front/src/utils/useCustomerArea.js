80c5bd534a201c9e708f4afbbaf5aaf42aa11aa7
Natfer84 <natita2804@hotmail.com>
2025-05-02 18:38:01 +0200

    cambio nombre a la hora de hacer el fetch

--- front/src/utils/useCustomerArea.js
+++ front/src/utils/useCustomerArea.js
@@ -46,7 +46,7 @@ export default function useCustomerArea() {
 
       const getUserFavorites = async () => {
         try {
-          const url = `https://REACT_APP_API_URL/favorites?username=${encodeURIComponent(username)}`; //HE CAMBIADO LOCALHOST A REACT_APP_API_URL
+          const url = `${process.env.REACT_APP_API_URL}/favorites?username=${encodeURIComponent(username)}`; //HE CAMBIADO LOCALHOST A REACT_APP_API_URL
       
           console.log("URL de la petición:", url);
 
