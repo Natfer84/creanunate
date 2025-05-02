4be037f594572162e5dbd517be40f0629187f5f9
Natfer84 <natita2804@hotmail.com>
2025-05-02 13:00:15 +0200

    realizado cambio base datos mongo

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,7 @@
 import express, { json, urlencoded } from "express";
 import cors from "cors";
 import { router } from "./routes/routes.js";
-import path from "path"; // para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
-import { fileURLToPath } from "url"; // para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
+
 
 const app = express();
 
@@ -12,19 +11,6 @@ app.use(cors());
 
 
 app.use(router);
-// Aquí configuro para que el backen sirva al front con la misma url que me da Railway, si no funciona, borrar.
-
-// Necesario para usar __dirname con ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// servir el frontend con build
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-//
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
-});
-//-----------------------------------------------------------------------------------------------------
 
 
 export default app;
