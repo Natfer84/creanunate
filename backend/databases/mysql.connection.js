e44f0dd0950a7cf846f7f3201d3cf43e896bda03
Natfer84 <natita2804@hotmail.com>
2025-05-03 14:06:19 +0200

    he creado u nas nuevas variables en backen de railway y las he modificado en la conexion con mysql en el codigo

--- backend/databases/mysql.connection.js
+++ backend/databases/mysql.connection.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 // Constante para que se pueda usar conectar en local o al servidor
 //const my_host = process.env.SQL_LOCALHOST;
-const my_host = process.env.SQL_LOCALHOST;
+const my_host = process.env.SQL_LOCARAILWAY;
 
 export default {
 
@@ -12,8 +12,8 @@ export default {
 		const pool = mysql.createPool({
 			host: my_host,
 			user: process.env.SQL_USER,
-			password: process.env.SQL_PASS,
-			database: process.env.SQL_BBDD,
+			password: process.env.SQL_PSS,
+			database: process.env.SQL_BBSS,
 			port: process.env.SQL_PORT,
 			waitForConnections: true,
 			connectionLimit: 20,
