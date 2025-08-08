b19b1a2ca785011c3e61f93d98bca06c6059e3cb
Natfer84 <natita2804@hotmail.com>
2025-08-08 11:35:50 +0200

    conectado con Aws, con fronten y mongo altalas

--- backend/App.js
+++ backend/App.js
@@ -5,7 +5,12 @@ import { router } from "./routes/routes.js";
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+  origin: 'http://tienda-creatividad-frontend.s3-website.eu-west-3.amazonaws.com', // dominio frontend de AWS
+  credentials: true,
+}));
+
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
